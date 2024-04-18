ee658184d9e3e374ddfd8155530f37fec505c5c8
giacomoborsellino <giacomoborsellino@hotmail.it>
2024-04-18 15:57:01 +0200

    check

--- src/corso-react/src/App.jsx
+++ src/corso-react/src/App.jsx
@@ -4,6 +4,9 @@ import Card from "./components/Card/Card";
 import cities from "./cities";
 
 function App() {
+  const alert = (title) => {
+    console.log("You clicked the city ", title);
+  };
   return (
     <>
       <div className="bg-white rounded-md m-6 p-6">
@@ -15,15 +18,23 @@ function App() {
         alt="intro"
         src={orn}
       />
+      <button
+        onClick={() => {
+          console.log("You clicked the city ", "citta");
+        }}>
+        Clicca
+      </button>
       <div className="flex flex-row justify-center items-center w-full m-6">
-        {cities.map((city) => (
-          <Card
-            key={city.id}
-            title={city.title}
-            img={city.img}
-            description={city.description}
-            isVisited={city.isVisited}></Card>
-        ))}
+        {cities
+          .filter((city) => city.isVisited)
+          .map((city) => (
+            <Card
+              key={city.id}
+              title={city.title}
+              img={city.img}
+              description={city.description}
+              isVisited={city.isVisited}></Card>
+          ))}
       </div>
     </>
   );
